a9574f3c0e58f6200d4574e2f8d256c7bca25a4a
Luka Barth <luka.costa@db1.com.br>
2025-04-09 14:25:55 -0300

    Módulo: classes de equivalência
    Nesse módulo foi discutido sobre quantidade de testes que devem ser criados, e por isso, apresentado o conceito de análise de valor limite para se ter uma ideia dessa quantidade

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
@@ -22,6 +24,17 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 
     public function getMaiorValor(): float
