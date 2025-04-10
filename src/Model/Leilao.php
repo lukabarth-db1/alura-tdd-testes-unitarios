85e98fb4dcd99f013580626337c5a4a550852db6
Luka Barth <luka.costa@db1.com.br>
2025-04-10 10:48:59 -0300

    Módulo: Test Driven Development
    Nesse módulo foi ensinado o conceito de TDD e que devemos implementar funcionalidades em pequenos passos, sempre guiados pelos testes

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -15,11 +15,40 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
+            return;
+        }
+
+        $totalLanceUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+
+        if ($totalLanceUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
+    }
+
+    private function ehDoUltimoUsuario(Lance $lance): bool
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
     }
 
     public function getLances(): array
     {
         return $this->lances;
+    }
+
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLanceUsuario = array_reduce($this->lances, function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+            if ($lanceAtual->getUsuario() == $usuario) {
+                return $totalAcumulado + 1;
+            }
+
+            return $totalAcumulado;
+        }, 0);
+
+        return $totalLanceUsuario;
     }
 }
